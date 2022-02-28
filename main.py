c187620c2dc275de2a0a3809f17e0823f356725b
Dana Huff <dana@hufflink.org>
2022-02-28 13:55:32 -0800

    Add table

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+    'Series-1':[1, 3, 4, 5, 7],
+    'Series-2':[10,30,40,100,120]
+}
 
 st.title("Our First Streamlit App")
 st.subheader("Introducing Streamlit in Animate Everything with Python")
 st.write('''This is our first web app
 Enjoy it!
 ''')
+df = pd.DataFrame(data)
+st.write(df)
