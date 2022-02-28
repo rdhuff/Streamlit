32167a58a3b79667c211a3b01f0b52ac08c63fdd
Dana Huff <dana@hufflink.org>
2022-02-28 13:25:34 -0800

    First streamlit app

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Our First Streamlit App")
+st.subheader("Introducing Streamlit in Animate Everything with Python")
+st.write('''This is our first web app
+Enjoy it!
+''')
